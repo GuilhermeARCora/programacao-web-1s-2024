cda0945f48219bb9b28a55e089d0ab6b7b352537
Guilherme Corá <guilhermearcora@gmail.com>
2024-03-14 11:24:13 -0300

    Add files via upload

---
+++ Lista1/Atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+// Declaração de variáveis numéricas
+let num_A = 5;
+let num_B = 3;
+
+// Operação de adição
+let resultado_adicao = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B}: ${resultado_adicao}`);
+
+// Operação de subtração
+let resultado_subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B}: ${resultado_subtracao}`);
+
+// Operação de multiplicação
+let resultado_multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B}: ${resultado_multiplicacao}`);
+
+// Operação de divisão
+let resultado_divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B}: ${resultado_divisao}`);
